50c23175a1606b759c2fc7d69d7b2dbec8c7b383
Michael Black <brushyfork23@gmail.com>
2017-05-08 23:51:47 -0700

    wip animation improvements, completed picker_pulse_single

--- libraries/PickerDisplay/PickerDisplay.cpp
+++ libraries/PickerDisplay/PickerDisplay.cpp
@@ -33,25 +33,42 @@ void PickerDisplay::startSeed(uint16_t seed) {
 void PickerDisplay::setColor(uint32_t color) {
   this->color = CRGB(color);
 }
+void queueAnimation(animation anim) {
+  if ( this->TotalSteps == 0 ) {
+    // current animation will never "complete", so transition immediately
+    this->setAnimation(anim);
+  } else {
+    this->queue = anim;
+  }
+}
 
-void setAnimation(animation anim) {
+void setAnimation(animation anim, bool clearQueue) {
   this->ActivePattern = anim;
+  this->Index = 0;
+  if ( clearQueue ) {
+    this->queue = NONE;
+  }
   switch (anim) {
     case BLACK:
-      fill_solid( this->leds, NUL_LEDS, CRGB::Black);
-      break;
-    case SOLID:
-      fill_solid( this->leds, NUL_LEDS, this->color);
+      Black();
       break;
     case PICKER_PREVIEW_INIT:
-      this->TotalSteps = 8;
-      this->Index = 0;
+      PickerPreviewInit();
+      break;
+    case PICKER_RESET:
+      PickerReset();
+      break;
+    case PICKER_PULSE:
+      PickerPulse();
       break;
     case PICKER_PULSE_SINGLE:
-      this->TotalSteps = 20;
-      this->Index = 0;
+      PickerPulseSingle();
       break;
+    case PICKER_CONFIRM:
+      PickerConfirm();
+    break;
     default:
+      this->TotalSteps = 0;
       break;
   }
 }
@@ -59,41 +76,39 @@ void setAnimation(animation anim) {
 // Change to the next animation in the queue
 void OnComplete() {
   if ( this->queue != NONE ) {
-    this->ActivePattern = this->queue;
+    this->setAnimation(this->queue);
     this->queue = NONE;
   }
 }
 
-// Update the pattern
 void Update()
 {
-    if(pushNextFrame.check())
-    {
-        lastUpdate = millis();
-        switch(ActivePattern)
-        {
-            case SOLID:
-            	SolidUpdate();
-            	break;
-            case PICKER_PREVIEW_INIT:
-            	PickerPreviewInitUpdate();
-            	break;
-            case PICKER_RESET:
-            	PickerResetUpdate();
-            	break;
-            case PICKER_PULSE:
-            	PickerPulseUpdate();
-            	break;
-            case PICKER_PULSE_SINGLE:
-            	PickerPulseSingleUpdate();
-            	break;
-            case PICKER_CONFIRM:
-              PickerConfirmUpdate();
-            default:
-                break;
-        }
-        show();
+  if ( pushNextFrame.check() ) {
+    lastUpdate = millis();
+    switch(ActivePattern) {
+      case SOLID:
+        SolidUpdate();
+        break;
+      case PICKER_PREVIEW_INIT:
+        PickerPreviewInitUpdate();
+        break;
+      case PICKER_RESET:
+        PickerResetUpdate();
+        break;
+      case PICKER_PULSE:
+        PickerPulseUpdate();
+        break;
+      case PICKER_PULSE_SINGLE:
+        PickerPulseSingleUpdate();
+        break;
+      case PICKER_CONFIRM:
+        PickerConfirmUpdate();
+      default:
+          break;
     }
+    this->Increment();
+    show();
+  }
 }
 
 // Increment the Index and reset at the end
@@ -109,15 +124,177 @@ void Increment()
 
 
 /**
-	ANIMATIONS
+  ANIMATIONS
 **/
+
+void Black() {
+    fill_solid( this->leds, NUL_LEDS, CRGB::Black);
+    // TotalSteps must be set to 0 so that when queue() is called next it will instantly transition
+    this->TotalSteps = 0;
+}
+
+void SolidUpdate() {
+  fill_solid( this->leds, NUL_LEDS, this->color);
+}
+
+void PickerPreviewInit() {
+  this->TotalSteps = 8;
+}
 
 // over the course of 8 steps, fill in the display with the new color.
 void PickerPreviewInitUpdate()
 {
-	fill_solid()
 }
+
+void PickerReset() {
+  this->TotalSteps = 8;
+}
+
+// push out old color
+void PickerResetUpdate() {
+  // draw black pixel in center, expanding outward
+}
+
+void PickerPulse() {
+  this->TotalSteps = 20;
+}
+
+// fade entire display from bright to dim
+void PickerPulseUpdate() {
+}
+
+void PickerPulseSingle() {
+  this->TotalSteps = 16;
+  this->randCounter = random8();
+}
+
+// fade in a random pixel in and out.  When a pixel is 50% faded out, start fading another random pixel in.
+// step 1 - 12: fade in actor in; all others out
+// step 13 - 16: fade all out
 void PickerPulseSingleUpdate() {
-  // fade in, then out
-  this->Increment();
+  // every new cycle, choose a new random actor between 2,3 and 3,4
+  if ( this->Index = 0) {
+    this->actor2Index = this->actor1Index;
+    // clear previous tmp
+    this->tmp = CRGB(0,0,0);
+    // select new actor
+    uint16_t newAddr;
+    do {
+      newAddr = XY(2 + random8(1), 3 + random8(1));
+    } while (this->actor1Index == newAddr);
+    this->actor1Index = newAddr;
+  }
+
+  // if no color is set, cycle through a rainbow
+  if (!this->isColor()) {
+    // cycle previous actor's color
+    leds[this->actor2Index].setHue(this->randCounter);
+    // cycle new actor
+    this->tmp.setHue(this->randCounter++);
+  } else {
+    this->tmp = CRGB(this->color);
+  }
+
+  // fade all LEDs out
+  this->leds.fadeToBlackBy(16);
+
+  if (this->Index < 12) {
+    // for the first 3/4, fade actor in
+    this->tmp.addToRGB(16);
+  } else {
+    // for the last 1/4, fade actor out
+    this->tmp.fadeToBlackBy(16);
+  }
+
+  // update actor
+  leds[this->actor1Index] = this->tmp;
+}
+
+void PickerConfirm() {
+  this->TotalSteps = 20;
+}
+
+// do a sweet confimation animation.  Make it super cool and poofy
+void PickerConfirmUpdate() {
+
+}
+
+
+
+
+/**
+  XY grid helper methods taken from FastLED example:
+  https://github.com/FastLED/FastLED/blob/master/examples/XYMatrix/XYMatrix.ino
+**/
+
+// Helper functions for an two-dimensional XY matrix of pixels.
+// Simple 2-D demo code is included as well.
+//
+//     XY(x,y) takes x and y coordinates and returns an LED index number,
+//             for use like this:  leds[ XY(x,y) ] == CRGB::Red;
+//             No error checking is performed on the ranges of x and y.
+//
+//     XYsafe(x,y) takes x and y coordinates and returns an LED index number,
+//             for use like this:  leds[ XY(x,y) ] == CRGB::Red;
+//             Error checking IS performed on the ranges of x and y, and an
+//             index of "-1" is returned.  Special instructions below
+//             explain how to use this without having to do your own error
+//             checking every time you use this function.  
+//             This is a slightly more advanced technique, and 
+//             it REQUIRES SPECIAL ADDITIONAL setup, described below.
+
+// XY grid helper method initialization
+
+
+// Param for different pixel layouts
+
+//
+// Bonus vocabulary word: anything that goes one way 
+// in one row, and then backwards in the next row, and so on
+// is call "boustrophedon", meaning "as the ox plows."
+
+// This function will return the right 'led index number' for 
+// a given set of X and Y coordinates on your matrix.  
+// IT DOES NOT CHECK THE COORDINATE BOUNDARIES.  
+// That's up to you.  Don't pass it bogus values.
+//
+// Use the "XY" function like this:
+//
+//    for( uint8_t x = 0; x < kMatrixWidth; x++) {
+//      for( uint8_t y = 0; y < kMatrixHeight; y++) {
+//      
+//        // Here's the x, y to 'led index' in action: 
+//        leds[ XY( x, y) ] = CHSV( random8(), 255, 255);
+//      
+//      }
+//    }
+//
+//
+uint16_t XY( uint8_t x, uint8_t y)
+{
+  uint16_t i;
+  
+  if( kMatrixSerpentineLayout == false) {
+    i = (y * kMatrixWidth) + x;
+  }
+
+  if( kMatrixSerpentineLayout == true) {
+    if( y & 0x01) {
+      // Odd rows run backwards
+      uint8_t reverseX = (kMatrixWidth - 1) - x;
+      i = (y * kMatrixWidth) + reverseX;
+    } else {
+      // Even rows run forwards
+      i = (y * kMatrixWidth) + x;
+    }
+  }
+  
+  return i;
+}
+
+uint16_t XYsafe( uint8_t x, uint8_t y)
+{
+  if( x >= kMatrixWidth) return -1;
+  if( y >= kMatrixHeight) return -1;
+  return XY(x,y);
 }
--- libraries/PickerDisplay/PickerDisplay.h
+++ libraries/PickerDisplay/PickerDisplay.h
@@ -2,7 +2,6 @@
 Runs animation of an Adafruit NeoPixel FeatherWing
 
 
-
 */
 #ifndef Picker_Display_h
 #define Picker_Display_h
@@ -10,7 +9,40 @@ Runs animation of an Adafruit NeoPixel FeatherWing
 #include <FastLED.h>
 #include <Metro.h>
 
-#define NUM_LEDS 32
+const uint8_t kMatrixWidth = 4;
+const uint8_t kMatrixHeight = 8;
+const bool    kMatrixSerpentineLayout = true;
+// Set 'kMatrixSerpentineLayout' to false if your pixels are 
+// laid out all running the same way, like this:
+//
+//     0 >  1 >  2 >  3 >  4
+//                         |
+//     .----<----<----<----'
+//     |
+//     5 >  6 >  7 >  8 >  9
+//                         |
+//     .----<----<----<----'
+//     |
+//    10 > 11 > 12 > 13 > 14
+//                         |
+//     .----<----<----<----'
+//     |
+//    15 > 16 > 17 > 18 > 19
+//
+// Set 'kMatrixSerpentineLayout' to true if your pixels are 
+// laid out back-and-forth, like this:
+//
+//     0 >  1 >  2 >  3 >  4
+//                         |
+//                         |
+//     9 <  8 <  7 <  6 <  5
+//     |
+//     |
+//    10 > 11 > 12 > 13 > 14
+//                        |
+//                        |
+//    19 < 18 < 17 < 16 < 15
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
 #define BRIGHTNESS 32
 
 // Animation types supported:
@@ -24,6 +56,7 @@ enum animation {
 	PICKER_PULSE_SINGLE,
 	PICKER_CONFIRM
 };
+
 
 class PickerDisplay {
 
@@ -41,12 +74,14 @@ class PickerDisplay {
     	// runs the animation; transitions to next 
     	void update();
     	// immediately change the current animation
-    	void setAnimation(animation anim);
-    	// queue an animation to display next
+    	void setAnimation(animation anim, bool clearQeue = true);
+    	// queue an animation to display when the current completes
     	void queueAnimation(animation anim);
 
 	private:
-		CRGB leds[NUM_LEDS];
+
+		CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
+		CRGB* const leds( leds_plus_safety_pixel + 1);
 
 	    animation ActiveAnimation;  // which animation is running
     
@@ -65,6 +100,12 @@ class PickerDisplay {
     	uint16_t TotalSteps;  // total number of steps in the pattern
     	uint16_t Index;  // current step within the pattern
     	CRGB color;
+
+    	// animation helper vars
+    	uint8_t randCounter;
+    	uint8_t actor1Index;
+    	uint8_t actor2Index;
+    	CRGB tmp;
     
 };
 
